3804fcd8543a425fe47ae766b5711478816fc89c
puneeth <gudipati.puneeth.35.3@mountblue.tech>
2026-04-22 15:00:39 +0530

    Refactored

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.get("/api/:date?", (req, res) => {
   })
 })
 
+
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
